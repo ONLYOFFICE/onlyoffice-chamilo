a9bb7b99957a76b18d316e1584e22f17f1153976
hellonadya <n.knyazeva118@gmail.com>
2021-02-19 16:28:26 +0300

    Update english.php

--- lang/english.php
+++ lang/english.php
@@ -4,10 +4,10 @@
  * 
  */
 $strings["plugin_title"] = "ONLYOFFICE";
-$strings['plugin_comment'] = "ONLYOFFICE connector allows you to view, edit and collaborate on text documents, spreadsheets and presentations within Chamilo using ONLYOFFICE Docs.";
+$strings['plugin_comment'] = "ONLYOFFICE connector allows you to view, edit, and collaborate on text documents, spreadsheets, and presentations within Chamilo using ONLYOFFICE Docs.";
 $strings["enableOnlyofficePlugin"] = "Enable";
 $strings["documentServerUrl"] = "Document Editing Service address";
-$strings["openByOnlyoffice"] = "Open by onlyoffice";
+$strings["openByOnlyoffice"] = "Open with ONLYOFFICE";
 $strings["createNew"] = "Create new";
 $strings["title"] = "Title";
 $strings["chooseFileFormat"] = "Choose file format";
