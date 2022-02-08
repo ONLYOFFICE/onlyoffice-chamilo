ec0c56d1955f0b37acbe12312f4bb74d7f22a49a
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2022-02-08 13:09:50 +0500

    fix: folder name

--- pack.sh
+++ pack.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-rm -rf ./deploy
+rm -rf ./onlyoffice
 rm ./RELEASE.md
 
 cwd=$(pwd)
 git submodule update --init --recursive
 
-mkdir ./deploy
-rsync -av --exclude='deploy' ./ ./deploy/
+mkdir ./onlyoffice
+rsync -av --exclude='onlyoffice' ./ ./onlyoffice/
 cd ./deploy
 
 rm -rf ./.github
 rm -rf ./.git/
 rm ./.gitignore
 rm ./pack.sh
+rm ./.gitmodules
 
 cd $cwd
 awk '/## [0-9]/{p++} p; /## [0-9]/{if (p > 1) exit}' CHANGELOG.md | awk 'NR>2 {print last} {last=$0}' > RELEASE.md
-zip onlyoffice.zip -r deploy
